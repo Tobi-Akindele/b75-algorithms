a3babc0344731a312278866d9f7da416def563d9
Tobi-Akindele <oyindamolaakindele3@icloud.com>
2022-06-29 14:54:47 +0100

    Contain Duplicate (int array)

--- src/arrays/ContainsDuplicate.java
+++ src/arrays/ContainsDuplicate.java
@@ -13,11 +13,11 @@ public class ContainsDuplicate {
 		System.out.println(cd.containsDuplicate(new int[] {1,2,3,1})); // should return false;
 		System.out.println();
 		
-		System.out.println("Test case one: ");
+		System.out.println("Test case two: ");
 		System.out.println(cd.containsDuplicate(new int[] {1,2,3,4})); // should return false;
 		System.out.println();
 		
-		System.out.println("Test case one: ");
+		System.out.println("Test case three: ");
 		System.out.println(cd.containsDuplicate(new int[] {1,1,1,3,3,4,3,2,4,2})); // should return false;
 		System.out.println();
 	}
