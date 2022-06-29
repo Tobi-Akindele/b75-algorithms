5f82381c89adb15046849856fe0d315513bba7f6
Tobi-Akindele <oyindamolaakindele3@icloud.com>
2022-06-29 20:36:01 +0100

    Maximum Subarray

---
+++ src/arrays/ProductofArrayExceptSelf.java
@@ -0,0 +1,37 @@
+package arrays;
+
+import java.util.Arrays;
+
+public class ProductofArrayExceptSelf {
+
+	public static void main(String[] args) {
+		
+		ProductofArrayExceptSelf obj = new ProductofArrayExceptSelf();
+		
+		int[] nums = {1,2,3,4};
+		System.out.println(Arrays.toString(obj.productExceptSelf(nums))); // should return [24, 12, 8, 6]
+	}
+
+	public int[] productExceptSelf(int[] nums) {
+
+		int[] output = new int[nums.length];
+
+		output[0] = 1;
+
+		// left prefix product
+		for (int i = 1; i < nums.length; i++) {
+			output[i] = output[i - 1] * nums[i - 1];
+		}
+
+		int product = 1;
+
+		for (int i = nums.length - 1; i >= 0; i--) {
+
+			output[i] = output[i] * product;
+
+			product *= nums[i];
+		}
+
+		return output;
+	}
+}
