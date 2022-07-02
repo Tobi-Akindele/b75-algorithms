90534dd0ffc19b5cdfc8bc22c9d330db8270f07e
Tobi-Akindele <oyindamolaakindele3@icloud.com>
2022-07-02 19:24:12 +0100

    Sum of Two Integers

---
+++ src/arrays/SumOfTwoIntegers.java
@@ -0,0 +1,18 @@
+package arrays;
+
+public class SumOfTwoIntegers {
+
+	public static void main(String[] args) {
+		System.out.println(new SumOfTwoIntegers().getSum(2, 3)); // should return 5
+	}
+
+	public int getSum(int a, int b) {
+
+		while(b != 0) {
+			int tmp = (a & b) << 1;
+			a = a ^ b;
+			b = tmp;
+		}
+		return a;
+	}
+}
