4d87b1c2a82e838979ed90eef46a75993c713fe2
Tobi-Akindele <oyindamolaakindele3@icloud.com>
2022-07-27 17:42:04 +0100

    Valid Parathensis

---
+++ src/strings/ValidParenthesis.java
@@ -0,0 +1,36 @@
+package strings;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.HashMap;
+import java.util.Map;
+
+public class ValidParenthesis {
+
+	public static void main(String[] args) {
+System.out.println(new ValidParenthesis().isValid("()[]"));
+	}
+
+	public boolean isValid(String s) {
+		Deque<String> stack = new ArrayDeque<>();
+
+		Map<String, String> closeToOpen = new HashMap<>();
+		closeToOpen.put(")", "(");
+		closeToOpen.put("]", "[");
+		closeToOpen.put("}", "{");
+
+		for (int i = 0; i < s.length(); i++) {
+			if(closeToOpen.containsKey(Character.toString(s.charAt(i)))) {
+				if(!stack.isEmpty() && 
+						stack.peek().equals(closeToOpen.get(Character.toString(s.charAt(i))))) {
+					stack.pop();
+				} else {
+					return false;
+				}
+			} else {
+				stack.push(Character.toString(s.charAt(i)));
+			}
+		}
+		return stack.isEmpty();
+	}
+}
