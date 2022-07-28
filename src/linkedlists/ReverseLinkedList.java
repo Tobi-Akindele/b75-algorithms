947ef16bef1b2201c0cd200f141a9cbd0f8476f8
Tobi-Akindele <oyindamolaakindele3@icloud.com>
2022-07-28 20:43:22 +0100

    Reverse Linked List

--- src/linkedlists/ReverseLinkedList.java
+++ src/linkedlists/ReverseLinkedList.java
@@ -3,8 +3,36 @@ package linkedlists;
 public class ReverseLinkedList {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
 	}
 
+	public ListNode reverseList(ListNode head) {
+		ListNode prev = null;
+		ListNode curr = head;
+		
+		while (curr != null) {
+			ListNode next = curr.next; // temp variable
+			curr.next = prev;
+			prev = curr;
+			curr = next;
+		}
+		return prev;
+	}
+
+	public class ListNode {
+		int val;
+		ListNode next;
+
+		ListNode() {
+		}
+
+		ListNode(int val) {
+			this.val = val;
+		}
+
+		ListNode(int val, ListNode next) {
+			this.val = val;
+			this.next = next;
+		}
+	}
 }
